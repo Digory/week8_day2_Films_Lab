4734854289d9951071f2459237b05e0e1cd14424
Joanna Gora <joanna.gora@protonmail.com>
2018-08-14 15:52:38 +0100

    created and tested update method

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,11 +1,19 @@
 import db.DBHelper;
 import models.Director;
+import models.Film;
 
 public class Runner {
 
     public static void main(String[] args) {
 
-        Director director = new Director("Peter Jackson");
-        DBHelper.save(director);
+        Director directorPeter = new Director("Peter Jackson");
+        DBHelper.save(directorPeter);
+        Film film1 = new Film("Hobbit", directorPeter, 12);
+        DBHelper.save(film1);
+        film1.setTitle("Lord of the Rings");
+        DBHelper.update(film1);
+        directorPeter.setName("Peter Smackson");
+        DBHelper.update(directorPeter);
     }
+
 }
