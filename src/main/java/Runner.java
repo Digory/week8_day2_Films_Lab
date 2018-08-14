2ae840b4e7dd7b2b33fd1136eb8da5bd5ff1c92d
Joanna Gora <joanna.gora@protonmail.com>
2018-08-14 16:02:19 +0100

    created and tested findById method

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -9,11 +9,15 @@ public class Runner {
         Director directorPeter = new Director("Peter Jackson");
         DBHelper.save(directorPeter);
         Film film1 = new Film("Hobbit", directorPeter, 12);
+        Film film2 = new Film("Hobbit 2", directorPeter, 12);
         DBHelper.save(film1);
+        DBHelper.save(film2);
         film1.setTitle("Lord of the Rings");
         DBHelper.update(film1);
         directorPeter.setName("Peter Smackson");
         DBHelper.update(directorPeter);
+        DBHelper.delete(film2);
+        Director foundDirector = DBHelper.findById(Director.class, directorPeter.getId());
     }
 
 }
--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -1,8 +1,10 @@
 package db;
 
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 public class DBHelper {
     private static Transaction transaction;
@@ -34,5 +36,34 @@ public class DBHelper {
         } finally {
             session.close();
         }
+    }
+
+    public static void delete(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.delete(object);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static <T> T findById(Class classType, int id){
+        session = HibernateUtil.getSessionFactory().openSession();
+        T result = null;
+        try {
+            Criteria cr = session.createCriteria(classType);
+            cr.add(Restrictions.eq("id", id));
+            result = (T) cr.uniqueResult();
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return result;
     }
 }
